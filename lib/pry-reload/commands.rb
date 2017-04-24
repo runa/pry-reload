8a77e762d99c88159fa09e556d48f04315fc18ea
Billy.Zheng <zw963@163.com>
2017-04-24 23:44:36 +0800

    require pry in commands.rb

--- lib/pry-reload/commands.rb
+++ lib/pry-reload/commands.rb
@@ -1,7 +1,9 @@
-Pry.hooks.add_hook(:before_session, "reload-init") do
+require 'pry'
+
+Pry.hooks.add_hook(:before_session, 'reload-init') do
   PryReload::Watch.instance
 end
 
-Pry::Commands.block_command "reload!", "Reload changed files since the session started" do 
+Pry::Commands.block_command 'reload!', 'Reload changed files since the session started' do
   PryReload::Watch.instance.reload!(output)
 end
